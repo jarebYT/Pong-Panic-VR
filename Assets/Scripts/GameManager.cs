f89d4ca8b83c6e523b0f6cae6f864721163b3119
Raphaël Bedleem <rbedleem@myges.fr>
2026-01-15 15:52:38 +0100

    Add GameManager and PingPongManager scripts
    GameManager now have basic functions for the game like loadScene and Quit and will contain the logic of changing scene and set the Player's movement.
    PingPongManager will contain the rules of the main game.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion;
+
+
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Additive);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetLocomotion(bool locomotion)
+    {
+        LocomotionMediator locomotionMediator = FindFirstObjectByType<LocomotionMediator>();
+
+        if (locomotion)
+        {
+            locomotionMediator.enabled = true;
+            Debug.Log("Locomotion Enabled");
+        }
+        else
+        {
+            locomotionMediator.enabled = false;   
+            Debug.Log("Locomotion Disabled");
+        }
+    }
+}
