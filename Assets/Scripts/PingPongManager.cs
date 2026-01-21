2e2d4d52ed869a970e7192073d1de89818776a99
Raphaël Bedleem <rbedleem@myges.fr>
2026-01-21 17:57:12 +0100

    [+]main Game : add GameManager singleton and update PingPongManager
    Introduced a singleton pattern to GameManager for global access and persistence across scenes. Updated PingPongManager to use GameManager.Instance for locomotion control.

--- Assets/Scripts/PingPongManager.cs
+++ Assets/Scripts/PingPongManager.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion;
 
 public class PingPongManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        GameManager.Instance.SetLocomotion(false);
         
     }
 
