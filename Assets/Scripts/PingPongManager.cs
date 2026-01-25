fdbf324786dbc6739a4c781910f7e7351cc50d83
Raphaël Bedleem <rbedleem@myges.fr>
2026-01-25 16:58:25 +0100

    Add side colliders and game state logic to PingPongManager
    Introduced player and enemy side colliders to the scene. Added serialized fields for ball, paddles, and scores. Implemented a basic game state machine (Start, Game, Score, End)

--- Assets/Scripts/PingPongManager.cs
+++ Assets/Scripts/PingPongManager.cs
@@ -1,20 +1,94 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Locomotion;
+
 
 public class PingPongManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private BoxCollider playerSideCollider;
+    [SerializeField] private BoxCollider ennemySideCollider;
+    [SerializeField] private GameObject ball;
+    [SerializeField] private GameObject playerPaddle;
+    [SerializeField] private GameObject enemyPaddle;
+    [SerializeField] private int playerScore;
+    [SerializeField] private int enemyScore;
+    enum GameState
+    {
+        Start,
+        Game,
+        Score,
+        End
+    }
+
+    private GameState currentState;
+
     void Start()
     {
-        GameManager.Instance.SetLocomotion(false);
-        
+        //Set la position des joueurs et/ou éléments de jeu
+        //mettre GameState à Start avec un élément ex:bouton
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (currentState)
+        {
+            case GameState.Start:
+                Service();
+                break;
+            case GameState.Game:
+                Game();
+                break;
+            case GameState.Score:
+                Score();
+                break;
+            case GameState.End:
+                End();
+                break;
+            default:
+                break;
+        }
         
+    }
+
+    void Service()
+    {
+        //Lancer la balle
+        //Affichage de qui démarre le service
+
+        //Gérer le service de la balle
+
+        //Si le serive est bon, passer à l'état Game
+        currentState = GameState.Game;
+    }
+
+    void Game()
+    {
+        //Jeu en cours
+        //ne peux plus attraper la balle pendant le jeu
+        ball.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>().enabled = false;
+
+        //Vérifier si un joueur a marqué
+        currentState = GameState.Score;
+
+        //Vérifier si la partie est terminée
+        if (playerScore >= 11 || enemyScore >= 11)
+        {
+            currentState = GameState.End;
+        }
+    }
+
+    int Score()
+    {
+        //Gérer le score
+        return 0;
+    }
+
+    void End()
+    {
+        //Afficher le gagnant
+        //Proposer de rejouer ou de quitter
+        SceneManager.LoadScene("MainMenu");
     }
 }
