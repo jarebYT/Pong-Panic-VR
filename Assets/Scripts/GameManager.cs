c6aff30950225b5a1ba9fc2bfa94e1acb3311b62
Raphaël Bedleem <rbedleem@myges.fr>
2026-02-01 11:43:47 +0100

    [-]mainGame: Remove unworking locomotion function from The GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,59 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Locomotion;
 
 
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;
-    public static GameManager Instance => instance;
-    private void Awake() {
-        if (instance == null) {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        } else {
-            Destroy(gameObject);
-        }
-    }
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SceneManager.LoadScene(2, LoadSceneMode.Additive);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Choisir la scène à charger au démarrage du jeu
+        // LoadScene(2);
     }
 
     public void LoadScene(int sceneIndex)
     {
-        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        // SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        // Charger la scène spécifiée en mode additif mais va peux être casser quand il faudra changer de scène
         SceneManager.LoadScene(sceneIndex, LoadSceneMode.Additive);
     }
 
     public void ExitGame()
     {
         Application.Quit();
-    }
-
-    public void SetLocomotion(bool locomotion)
-    {
-        LocomotionMediator locomotionMediator = FindFirstObjectByType<LocomotionMediator>();
-
-        if (locomotion)
-        {
-            locomotionMediator.enabled = true;
-            Debug.Log("Locomotion Enabled");
-        }
-        else
-        {
-            locomotionMediator.enabled = false;   
-            Debug.Log("Locomotion Disabled");
-        }
     }
 }
