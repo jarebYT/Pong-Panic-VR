c70e8872449ab96ea02c3ae15401815ca19dd511
Sonny Naidja <133282829+NASonny@users.noreply.github.com>
2026-05-21 22:22:24 +0200

    [~] feature/ui: native keyboard added
    four hours just worked on this just for one line needed to get the native keyboard..

--- Assets/Scripts/UI/InputFieldKeyboard.cs
+++ Assets/Scripts/UI/InputFieldKeyboard.cs
@@ -1,36 +1,49 @@
 using TMPro;
 using UnityEngine;
+
 public class InputFieldKeyboardHook : MonoBehaviour
 {
     public GameObject keyboard;
     public Transform playerCamera;
-
     private TMP_InputField field;
-
-    public Vector3 offset = new Vector3(0, -0.3f, 1.2f);
+    private bool keyboardActive = false;
 
     void Awake()
     {
         field = GetComponent<TMP_InputField>();
         field.onSelect.AddListener(OnSelected);
+        field.onDeselect.AddListener(OnDeselected);
+    }
+
+    void Update()
+    {
+        // Le clavier suit la tête du joueur en temps réel
+        if (keyboardActive && keyboard.activeSelf)
+        {
+            Vector3 position =
+                playerCamera.position +
+                playerCamera.forward * 1.2f +
+                playerCamera.up * -0.6f;
+
+            keyboard.transform.position = position;
+
+            Vector3 lookDir = keyboard.transform.position - playerCamera.position;
+            lookDir.y = 0;
+            keyboard.transform.rotation = Quaternion.LookRotation(lookDir);
+        }
     }
 
     void OnSelected(string text)
     {
         keyboard.SetActive(true);
-
+        keyboardActive = true;
         field.ActivateInputField();
+        keyboard.transform.localScale = new Vector3(0.003f, 0.003f, 0.003f);
+    }
 
-        Vector3 position =
-            playerCamera.position +
-            playerCamera.forward * 1.2f +
-            playerCamera.up * -0.3f;
-
-        keyboard.transform.position = position;
-
-        Vector3 lookDir = keyboard.transform.position - playerCamera.position;
-        lookDir.y = 0;
-
-        keyboard.transform.rotation = Quaternion.LookRotation(lookDir);
+    void OnDeselected(string text)
+    {
+        keyboard.SetActive(false);
+        keyboardActive = false;
     }
 }
